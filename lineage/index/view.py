3116368b1b0c0a923a1070a8b218d56a745aa282
frisi <frisi@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-16 08:01:42 +0000

    cache results for the view, independent of context, dependent on the key

--- lineage/index/view.py
+++ lineage/index/view.py
@@ -1,12 +1,15 @@
 from Products.Five.browser import BrowserView
 from zope.schema.interfaces import IVocabularyFactory
 from zope.component._api import getUtility
+from plone.memoize.view import memoize_contextless
 
 class ChildsiteView(BrowserView):
 
+    @memoize_contextless
     def titleForKey(self, key):
-        """returns the childsite's title for their vocabulary value
+        """returns the childsite's title for their vocabulary value.
         """
+
         vocab = getUtility(IVocabularyFactory, 'lineage.childsites')(self.context)
         try:
             return vocab.getTerm(key).title
