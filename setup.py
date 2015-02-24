37042440e1b5b64a8a5b5758d50fe7adae82ee58
Jens W. Klein <jk@kleinundpartner.at>
2015-02-24 18:25:02 +0100

    fix it

--- setup.py
+++ setup.py
@@ -3,35 +3,38 @@ import os
 
 version = '0.5.dev2'
 
-setup(name='lineage.index',
-      version=version,
-      description="Adds an index and metadata that allows to search for "
-                  "content of a collective.lineage childsite",
-      long_description=open("README.rst").read() + "\n" + 
-                       open("CHANGES.rst").read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        ],
-      keywords='',
-      author='Harald Friessnegger',
-      author_email='harald at webmeisterei dot com',
-      url='http://pypi.python.org/pypi/lineage.index',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['lineage'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'collective.lineage',
+setup(
+    name='lineage.index',
+    version=version,
+    description="Adds an index and metadata that allows to search for "
+                "content of a collective.lineage childsite",
+    long_description=open("README.rst").read() + "\n" +
+                     open("CHANGES.rst").read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+      "Framework :: Plone",
+      "Programming Language :: Python",
+      "Intended Audience :: Developers",
+      "License :: OSI Approved :: GNU General Public License (GPL)",
       ],
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    keywords='',
+    author='Harald Friessnegger',
+    author_email='harald at webmeisterei dot com',
+    url='http://pypi.python.org/pypi/lineage.index',
+    license='GPL',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['lineage'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'collective.lineage',
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
